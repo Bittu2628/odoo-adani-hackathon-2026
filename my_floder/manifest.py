02ec09401ae4739d760a2ce2913b423a7360a0d2
Bittu2628 <sirwanibhavesh7@gmail.com>
2025-12-27 09:55:59 +0530

    Create manifest.py

---
+++ my_floder/manifest.py
@@ -0,0 +1,11 @@
+{
+    'name': 'GearGuard',
+    'version': '1.0',
+    'summary': 'Maintenance Tracking System',
+    'category': 'Maintenance',
+    'author': 'GearGuard Team',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
